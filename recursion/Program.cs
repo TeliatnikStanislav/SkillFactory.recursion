6929126cc06659e44c4db0beee3ea722b14633e0
Stanislav <slava.telyatnik@mail.ru>
2023-07-27 18:21:10 +0200

    Добавьте файлы проекта.

---
+++ recursion/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace recursion
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Напишите что-то");
+            var str = Console.ReadLine();
+
+            Console.WriteLine("Укажите глубину эха");
+            var deep = int.Parse(Console.ReadLine());
+
+            Echo(str, deep);
+
+            Console.ReadKey();
+        }
+
+        static void Echo(string phrase, int deep)
+        {
+            Console.WriteLine(phrase);
+
+            if (deep > 1)
+            {
+                Echo(phrase, deep - 1);
+            }
+        }
+    }
+}
